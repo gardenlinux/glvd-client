6a9a058c8cee31072083b6b063b6ce372d209f14
Florian Wilhelm <florian.wilhelm02@sap.com>
2024-09-13 14:12:41 +0200

    wip

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
+
+type sourcePackageCve struct {
+	CveId                string `json:"cveId"`
+	SourcePackageName    string `json:"sourcePackageName"`
+	SourcePackageVersion string `json:"sourcePackageVersion"`
+	GardenlinuxVersion   string `json:"gardenlinuxVersion"`
+	IsVulnerable         bool   `json:"isVulnerable"`
+	CvePublishedDate     string `json:"cvePublishedDate"`
+}
 
 func main() {
 	resp, err := http.Get("https://glvd.ingress.glvd.gardnlinux.shoot.canary.k8s-hana.ondemand.com/v1/packages/vim")
@@ -17,4 +27,13 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(body))
+
+	var results []sourcePackageCve
+	err = json.Unmarshal(body, &results)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(results)
 }
